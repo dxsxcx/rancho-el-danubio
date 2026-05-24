1887fb543f386961988813814d23f6e41796849a
Daniel Santos Calderon <danielitosantos95@gmail.com>
2026-05-23 19:55:57 -0600

    preparado para deploy github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dxsxcx.github.io',
+  base: '/rancho-el-danubio',
   vite: {
     plugins: [tailwindcss()]
   }
